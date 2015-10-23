2c411961a3cc1276a3ba1a4b6d2a954abbb035f5
FAURE Adrien <adrien.faure2@gmail.com>
2015-10-23 20:55:34 +0200

    fixed after test :  typo and template, form is not send if it is invalid

--- ecom/ecom-web/src/main/webapp/js/controllers/InscriptionController.js
+++ ecom/ecom-web/src/main/webapp/js/controllers/InscriptionController.js
@@ -1,21 +1,29 @@
 var angular = require('angular');
 
-var InscriptionController = function($scope, memberService) {
+var InscriptionController = function ($scope, memberService) {
 
     $scope.user = {
-        login : "",
-        email : "",
-        firstName : "",
-        lastName : "",
-        password : "",
-        accountType : 'N'
+        login: "",
+        email: "",
+        firstName: "",
+        lastName: "",
+        password: "",
+        accountType: 'N'
     };
 
-    $scope.submit = function() {
-        console.log($scope.user);
+    $scope.error = "";
+
+    $scope.submit = function () {
+
         memberService.Create($scope.user).then(function (res) {
             console.log(res);
+            if (res.success == false) {
+                $scope.error = " Erreur, lors de l'inscription ";
+            } else {
+                $scope.error = " Enregistré ! ";
+            }
         });
+
     }
 
 
