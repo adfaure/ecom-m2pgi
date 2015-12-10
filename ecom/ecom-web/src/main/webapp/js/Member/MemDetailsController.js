7043a65a23b65f746e405fd5c799976dda18304f
AngieMoomin <olvera_16@hotmail.com>
2015-12-11 00:52:05 +0100

    Fixed french translations + details

--- ecom/ecom-web/src/main/webapp/js/Member/MemDetailsController.js
+++ ecom/ecom-web/src/main/webapp/js/Member/MemDetailsController.js
@@ -7,10 +7,11 @@ var MemDetailsController = function($scope, $location, $routeParams, apiToken, p
 	var memb = null;
 	$scope.edit = false;
 	$scope.addRIB = false;
+	$scope.actPSWfilled = false;
 	$scope.editPSW = false;
 	$scope.equalsPSW = false;
 	$scope.compteVendeur = false;
-	
+
 	$scope.sellerCheckBox = false;
 
 	$scope.user1 = {
@@ -26,7 +27,7 @@ var MemDetailsController = function($scope, $location, $routeParams, apiToken, p
 			}
 	};
 
-	
+
 	if(apiToken.isAuthentificated()) {
 		userID = apiToken.getUser().memberID;
 		if(apiToken.getUser().accountType == 'M'){
@@ -38,12 +39,10 @@ var MemDetailsController = function($scope, $location, $routeParams, apiToken, p
 		}
 		
 		
-		
-		//CREATE NEW SERVICE so the password won't be needed. 
-		//REMARK: IF I don't send the password, then it's set to NULL in the database when updating the member
+		//REMARK: This is done to get the password because IF I don't send the password, then it's set to NULL in the database when updating the member
 		memberService.GetById(userID).then(function(res){ memb = res;  fillOutInfo();});
 	}
-	
+
 
 	function fillOutInfo(){
 		if(memb != null){
@@ -77,42 +76,54 @@ var MemDetailsController = function($scope, $location, $routeParams, apiToken, p
 			fillOutInfo();
 			$scope.edit = false;
 		}
-		
-	}
 
+	}
+	
+
+	$scope.$watch('user1.pswActuel',function() {$scope.test();});
 	$scope.$watch('user1.pswNouveau',function() {$scope.test();});
 	$scope.$watch('user1.pswConfirmation',function() {$scope.test();});
 
 	$scope.test = function() {
 		$scope.equalsPSW = false;
-
+		$scope.actPSWfilled = false;
+		
+		if($scope.user1.pswNouveau == null || $scope.user1.pswConfirmation == null){
+			$scope.equalsPSW = false;
+		}
 		//If the passwords are not the same or if one of them is empty (and therefore the other as well)
-		if(($scope.user1.pswNouveau === $scope.user1.pswConfirmation) && $scope.user1.pswNouveau !== ''){
+		else if(($scope.user1.pswNouveau === $scope.user1.pswConfirmation) && $scope.user1.pswNouveau !== ''){
+			
 			$scope.equalsPSW = true;
 		}
-		else{
-			//mettre un msg d'alerte.
+		
+		
+		if($scope.user1.pswActuel == null){
+			$scope.actPSWfilled = false;
+		}
+		else if($scope.user1.pswActuel.length){
+			$scope.actPSWfilled = true;
 		}
 		
-		
+
 	};
 
 
 	$scope.save = function(){
-		
+
 		var actPSW = $scope.user1.pswActuel;
 		var newPSW = $scope.user1.pswNouveau;
 
 		delete $scope.user1.pswActuel;
 		delete $scope.user1.pswNouveau;
 		delete $scope.user1.pswConfirmation;
-		
-		
+
+
 		if($scope.editPSW){
-			
+
 			var prevPSW = $scope.user1.password;
 			$scope.user1.password = actPSW;			
-			
+
 			memberService.ChangePassword($scope.user1, newPSW).then(function(res){
 				if((res.success != null && !res.success) || !res){
 					alertService.add("alert-danger", " Erreur, le mot de passe actuel n'est pas correct", 2000);
@@ -121,49 +132,41 @@ var MemDetailsController = function($scope, $location, $routeParams, apiToken, p
 				else{
 					console.log(res);
 					updateMemSeller(res);
-					
+
 					$scope.editPSW = false;
 					//emptyPSWvalues();S
 					alertService.add("alert-success", "Mot de pass modifié! ", 2000);
 				}
 			});
-			
-			
+
+
 		}
 		else if($scope.addRIB){
-			
+
 			sellerService.CreateFromMember($scope.user1).then(function(res){
-				
+
 				if(res.success){
-					alertService.add("alert-success", "Upgrade done! ", 2000);
+					alertService.add("alert-success", "Promotion faite! ", 2000);
 					//if it's not successful then say it couldn't be.
-					
+
 					$scope.AccountType = "Vendeur";
 					$scope.addRIB = false;
 					$scope.compteVendeur = true;
-					
+
 					memberService.GetById(userID).then(function(res){ 
 						memb = res;  
 						apiToken.setUser(memb);
 						fillOutInfo(); 
 					});
-					
-					//updateMemSeller(res);
-					
-					//changeToken(res);
-					
-				}else{
-					alertService.add("alert-danger", " Erreur, the upgrade couldn't be done", 2000);
-				}
-				
-				
 
+				}else{
+					alertService.add("alert-danger", " Erreur, la promotion n'as pas pu être faite", 2000);
+				}
 			});
-			
-			
+
+
 		}else{
-			//A ce moment la, le password est ok, et on va mettre toute la info.
-			
+	
 			if($scope.user1.accountType == 'M'){
 				delete $scope.user1.sellerInfo;
 
@@ -174,18 +177,18 @@ var MemDetailsController = function($scope, $location, $routeParams, apiToken, p
 			}else{
 				sellerService.Update($scope.user1).then(function(res) {
 					updateMemSeller(res);
-					
+
 				});
 			}
-			
+
 			$scope.edit = false;
 		}
 	}
-	
+
 
 	function updateMemSeller(res){
 		console.log("res es " +res);
-		
+
 		if(res.user != null){
 			//because when upgrading a member, the res.user is returned.
 			memb = res.user;
@@ -193,17 +196,12 @@ var MemDetailsController = function($scope, $location, $routeParams, apiToken, p
 		else{
 			memb = res;
 		}
-		
+
 		fillOutInfo();
 	}
-	
-	function changeToken(res){
-		if(res){
-			apiToken.setUser(res);
-		}
-		
-	}
-	
+
+
+
 };
 
 module.exports = MemDetailsController;
