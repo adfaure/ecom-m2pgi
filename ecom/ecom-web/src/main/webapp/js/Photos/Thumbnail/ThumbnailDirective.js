ed9d1e716275add304323511ff6c54ab1d845c1c
FAURE Adrien <adrien.faure2@gmail.com>
2015-12-14 09:40:46 +0100

    fixed bug in thumbnail

--- ecom/ecom-web/src/main/webapp/js/Photos/Thumbnail/ThumbnailDirective.js
+++ ecom/ecom-web/src/main/webapp/js/Photos/Thumbnail/ThumbnailDirective.js
@@ -8,40 +8,49 @@ var thumbnailElementDirective = function ($compile, $location, apiToken, publicP
         },
         templateUrl: './js/Photos/Thumbnail/photoThumbnailTemplate.html',
         controller : function ($scope ) {
-            
+
+
             $scope.details = function (photoId) {
                 $location.path('/photos/details/' + photoId);
             };
 
             $scope.wish = function (photoID){
-                if(apiToken.isAuthentificated())
-                    publicPhoto.AddPhotoToWishList(photoID, user.memberID).then(function(res) {
-                        $scope.photo.wishlisted=true;
+                if(apiToken.isAuthentificated()) {
+                    var user = apiToken.getUser();
+                    publicPhoto.AddPhotoToWishList(photoID, user.memberID).then(function (res) {
+                        $scope.photo.wishlisted = true;
                     });
+                }
             };
 
             /* En attente du commit sur les photos*/
             $scope.unwish  = function (photoID){
-                if(apiToken.isAuthentificated())
-                    publicPhoto.RemovePhotoFromWishList(photoID, user.memberID).then(function(res) {
-                        $scope.photo.wishlisted=false;
+                if(apiToken.isAuthentificated()) {
+                    var user = apiToken.getUser();
+                    publicPhoto.RemovePhotoFromWishList(photoID, user.memberID).then(function (res) {
+                        $scope.photo.wishlisted = false;
                     });
+                }
             };
 
             $scope.like = function(photoID){
-                if(apiToken.isAuthentificated())
-                    publicPhoto.AddPhotoToLikeList(photoID, user.memberID).then(function(res) {
+                if(apiToken.isAuthentificated()) {
+                    var user = apiToken.getUser();
+                    publicPhoto.AddPhotoToLikeList(photoID, user.memberID).then(function (res) {
                         $scope.photo.liked = true;
                         $scope.photo.likes++;
                     });
+                }
             };
 
             $scope.unlike = function(photoID){
-                if(apiToken.isAuthentificated())
-                    publicPhoto.RemovePhotoFromLikeList(photoID, user.memberID).then(function(res) {
-                        $scope.photo.liked=false;
+                if(apiToken.isAuthentificated()) {
+                    var user = apitToken.getUser();
+                    publicPhoto.RemovePhotoFromLikeList(photoID, user.memberID).then(function (res) {
+                        $scope.photo.liked = false;
                         $scope.photo.likes--;
                     });
+                }
             }
         }
     };
